b699cbd1188a8f56884e1634f4255fb144b91ea0
tmrupp <tmrupp@umich.edu>
2023-04-30 11:28:44 -0700

    more context in shop

--- Shop.gd
+++ Shop.gd
@@ -12,26 +12,34 @@ class Option:
 	var button
 	var stat
 	var shop
+	var label
 	
-	func _init(button, stat, shop):
+	func _init(label, button, stat, shop):
 		self.button = button
 		self.stat = stat
 		self.shop = shop
 		self.cost = 1
+		self.label = label
 		button.pressed.connect(buy)
+		show()
+		
+	func show():
+		button.text = "Increase " + label + "\ncost: " + str(cost) + "\ncurrent: " + str(stat.capacity)
+		
 	
 	func can_buy():
-		print("button=", button)
-		print("shop=", shop)
-		print("cost=", cost)
+#		print("button=", button)
+#		print("shop=", shop)
+#		print("cost=", cost)
 		button.disabled = shop.coins < cost
-		print("disabled=", button.disabled)
+#		print("disabled=", button.disabled)
 	
 	func buy():
 		shop.delta_coins(-cost)
 		cost *= 2
 		stat.capacity += 1
 		shop.update_shop()
+		show()
 
 func _on_open_shop():
 #	print("pressed")
@@ -50,9 +58,9 @@ func _ready():
 	options.visible = false
 	delta_coins(100)
 	$Top/OpenShop.pressed.connect(_on_open_shop)
-	strength_option = Option.new($UpgradeOptions/Strength, player.strength, self)
-	speed_option = Option.new($UpgradeOptions/Speed, player.speed, self)
-	souls_option = Option.new($UpgradeOptions/Souls, player.souls, self)
+	strength_option = Option.new("strength", $UpgradeOptions/Strength, player.strength, self)
+	speed_option = Option.new("speed", $UpgradeOptions/Speed, player.speed, self)
+	souls_option = Option.new("soul capacity", $UpgradeOptions/Souls, player.souls, self)
 	options_list = [strength_option, speed_option, souls_option]
 	
 	
